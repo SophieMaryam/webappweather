fc9c5eda1fc8967d7dfe6bf32e5979308f9ad3f8
Sophie van Wersch <sophievanwersch@hotmail.com>
2020-05-06 13:38:17 +0200

    Import and add store to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
+import { store } from "./store";
 import { BootstrapVue } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
@@ -14,5 +15,6 @@ Vue.component("font-awesome-icon", FontAwesomeIcon);
 Vue.config.productionTip = false;
 
 new Vue({
+  store,
   render: h => h(App)
 }).$mount("#app");
