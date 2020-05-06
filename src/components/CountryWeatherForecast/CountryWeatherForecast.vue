20f10ed62293b52fe33b846d2b89a50ef809ccb1
Sophie van Wersch <sophievanwersch@hotmail.com>
2020-05-06 16:47:46 +0200

    Add loader depending on api data received

--- src/components/CountryWeatherForecast/CountryWeatherForecast.vue
+++ src/components/CountryWeatherForecast/CountryWeatherForecast.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="">
+  <div class="text-center justify-content-center col-md-12">
+    <div v-if="!checkDataReceived" class="loader"></div>
+    <div v-else>
+      <p>working</p>
+    </div>
   </div>
 </template>
 
@@ -8,6 +12,11 @@ import Vue from "vue";
 
 export default Vue.extend({
   name: "CountryWeatherForecast",
+  computed: {
+    checkDataReceived(): boolean {
+      return this.$store.state.dataReceived;
+    }
+  },
 });
 </script>
 
