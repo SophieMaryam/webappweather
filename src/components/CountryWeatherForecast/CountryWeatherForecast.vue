1dad7acfb29e8f038e0b318bd24434469bb9b7d8
Sophie van Wersch <sophievanwersch@hotmail.com>
2020-05-06 13:02:58 +0200

    Create CountryWeatherForecast.vue

---
+++ src/components/CountryWeatherForecast/CountryWeatherForecast.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="">
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "CountryWeatherForecast",
+});
+</script>
+
+<style scoped src="./CountryWeatherForecast.css"></style>
