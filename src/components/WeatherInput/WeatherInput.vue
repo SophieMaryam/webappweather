ee58d88764fc60016a17f5580a8a0c85659e3fea
Sophie van Wersch <sophievanwersch@hotmail.com>
2020-05-06 13:40:56 +0200

    Create city input field

--- src/components/WeatherInput/WeatherInput.vue
+++ src/components/WeatherInput/WeatherInput.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>
+  <div class="searchbar-container">
+    <b-form @submit.prevent="onSubmit(city)" class="mx-auto" inline>
+      <div class="form-row form-content col-md-12">
+        <div class="form-group row col-md-9">
+          <b-input-group>
+            <b-input
+              class="country-input"
+              placeholder="Please enter your location here..."
+              v-model="city"
+              required
+            />
+            <b-input-group-append @click="onSubmit(city)">
+              <span class="input-group-text">
+                <font-awesome-icon icon="search" />
+              </span>
+            </b-input-group-append>
+          </b-input-group>
+        </div>
+      </div>
+    </b-form>
   </div>
 </template>
 
@@ -7,7 +26,17 @@
 import Vue from "vue";
 
 export default Vue.extend({
-  name: "WeatherInput"
+  name: "WeatherInput",
+  data() {
+    return {
+      city: ""
+    };
+  },
+  methods: {
+    onSubmit(city: string) {
+      console.log(city)
+    },
+  },
 });
 </script>
 
