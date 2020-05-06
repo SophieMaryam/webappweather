d26fba1a5957d96d165aa42df555868eed73ce43
Sophie van Wersch <sophievanwersch@hotmail.com>
2020-05-06 15:48:19 +0200

    Update weather service url
    Gets 7 days

--- src/services/weatherService.ts
+++ src/services/weatherService.ts
@@ -3,7 +3,7 @@ import { API_KEY } from "@/common/constant";
 
 export function getWeatherApi(payload: { country: string; city: string }) {
   const { country, city } = payload;
-  const CURRENT_WEATHER = `https://api.weatherbit.io/v2.0/forecast/hourly/current?city=${city},${country}&key=${API_KEY}&hours=48`;
+  const CURRENT_WEATHER = `https://api.weatherbit.io/v2.0/forecast/daily/current?city=${city},${country}&key=${API_KEY}&days=7`;
   return axios
     .get(CURRENT_WEATHER)
     .then(response => {
