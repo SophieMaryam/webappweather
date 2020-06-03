75d6a93c9bb9957be43d3faf83236d9608656f9f
Sophie van Wersch <sophievanwersch@hotmail.com>
2020-06-03 11:40:33 +0200

    Update WeatherInput.spec.ts

--- src/components/WeatherInput/__tests__/WeatherInput.spec.ts
+++ src/components/WeatherInput/__tests__/WeatherInput.spec.ts
@@ -1,75 +1,85 @@
 import { shallowMount, createLocalVue } from "@vue/test-utils";
 import Vuex from "vuex";
-import WeatherInput from "../WeatherInput.vue"
-import BootstrapVue from 'bootstrap-vue';
+import WeatherInput from "../WeatherInput.vue";
+import BootstrapVue from "bootstrap-vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSearch, faCloudSun } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { weatherInputMock } from './__mocks__/WeatherInput' 
+import { weatherInputMock } from "./__mocks__/WeatherInput";
 const localVue = createLocalVue();
 
 localVue.use(Vuex);
-localVue.use(BootstrapVue)
-localVue.component("font-awesome-icon", FontAwesomeIcon)
+localVue.use(BootstrapVue);
+localVue.component("font-awesome-icon", FontAwesomeIcon);
 library.add(faSearch, faCloudSun);
 
 describe("WeatherInput", () => {
   let store: any;
-  // let actions: any;
   let wrapper: any;
 
+
   beforeEach(() => {
-    // actions = {
-    //   updateWeather: jest.fn(() => Promise.resolve())
-    // };
     store = new Vuex.Store(weatherInputMock);
-   
+
     wrapper = shallowMount(WeatherInput, {
       store,
       localVue,
       data() {
         return {
           city: "",
-          countrySelected: ""
-        }
-      }
+          countrySelected: "",
+        };
+      },
     });
   });
 
-  it("sets city and countrySelected data", () => {
-    wrapper.setData({city: "Utrecht", countrySelected: "NL"})
-    expect(wrapper.vm.city).toBe("Utrecht")
-    expect(wrapper.vm.countrySelected).toBe("NL")
-
+  it("sets city and countrySelected data properties", () => {
+    wrapper.setData({ city: "Utrecht", countrySelected: "NL" });
+    expect(wrapper.vm.city).toBe("Utrecht");
+    expect(wrapper.vm.countrySelected).toBe("NL");
   });
 
-  it('form submit triggers the onSubmit method with city and country data', () => {
-    const form = wrapper.find("#form");
-    form.trigger('submit.prevent');
-    const spyOnSubmit = jest.spyOn(wrapper.vm, 'onSubmit');
-    wrapper.vm.onSubmit({city: "Amsterdam", countrySelected: "NL"});
-    expect(spyOnSubmit).toBeCalledWith({ city: "Amsterdam", countrySelected: "NL"});
+  it("triggers the onSubmit method with args (city and countrySelected) when form is submitted", () => {
+    wrapper.find("#form").trigger("submit.prevent");
+    const spyOnSubmit = jest.spyOn(wrapper.vm, "onSubmit");
+    wrapper.vm.onSubmit({ city: "Amsterdam", countrySelected: "NL" });
+    expect(spyOnSubmit).toBeCalledWith({
+      city: "Amsterdam",
+      countrySelected: "NL",
+    });
   });
 
-  it('sets b-input as Amsterdam', () => {
-    const cityInput = wrapper.find('.city')
-    cityInput.value = "Amsterdam"
-    expect(cityInput.value).toBe("Amsterdam")
+  it("sets b-input as Amsterdam", () => {
+    const city = wrapper.find(".city");
+    city.value = "Amsterdam";
+    expect(city.value).toBe("Amsterdam");
   });
 
-  it('sets b-form-select as NL', () => {
-    const countrySelected = wrapper.find('.countrySelected')
-    countrySelected.value = "NL"
-    expect(countrySelected.value).toBe("NL")
+  it("sets b-form-select as NL", () => {
+    const countrySelected = wrapper.find(".countrySelected");
+    countrySelected.value = "NL";
+    expect(countrySelected.value).toBe("NL");
   });
 
-  it('expect sortCountryCodes method to be called on created', () => {
-    const spyOnSortCountryCodes = jest.spyOn(wrapper.vm, 'sortCountryCodes');
+  it("expects sortCountryCodes method to be called on created", () => {
+    const spyOnSortCountryCodes = jest.spyOn(wrapper.vm, "sortCountryCodes");
     wrapper.vm.sortCountryCodes();
     expect(spyOnSortCountryCodes).toHaveBeenCalled();
   });
 
-  it("dispatch an action when form is submitted", async () => {
-    expect(weatherInputMock.actions.updateWeather).toHaveBeenCalled()
-  })
+  it("calls store action 'updateWeather' when form is submitted", () => {
+    wrapper.find("#form").trigger("submit.prevent");
+    expect(weatherInputMock.actions.updateWeather).toHaveBeenCalled();
+  });
+
+  it("dispatches an action when a button is clicked", async () => {
+    store.dispatch = jest.fn()
+    jest.spyOn(wrapper.vm, "onSubmit");
+    wrapper.vm.onSubmit({ city: "Amsterdam", countrySelected: "NL" });
+    expect(store.dispatch).toBeCalled();
+    // expect(store.dispatch).toHaveBeenCalledWith('updateWeather', {
+    //   city: "Amsterdam",
+    //   countrySelected: "NL"
+    // });
+  });
 });
