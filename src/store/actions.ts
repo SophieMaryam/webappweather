0aa658979870c67eb7b335a8c24e4d2143a9a1ff
Sophie van Wersch <sophievanwersch@hotmail.com>
2020-06-03 13:54:41 +0200

    Call axios.get directly in action

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,16 +1,22 @@
-import { getWeatherApi } from "../services/weatherService";
+import { getWeatherApi } from '@/services/weatherService';
+import axios from 'axios';
 export const actions = {
   async updateWeather(
     context: { commit: (arg0: string, arg1: string) => void },
-    payload: { country: string; city: string }
+    payload: { countrySelected: string; city: string }
   ) {
-    await getWeatherApi(payload)
-      .then(res => {
-        console.log(res.data);
-        context.commit("UPDATE_WEATHER", res);
+    const { countrySelected, city } = payload;
+    const CURRENT_WEATHER = `https://api.weatherbit.io/v2.0/forecast/daily/current?city=${city},${countrySelected}&key=c9d0f8edb16446578c9047b2eda26642&days=7`;
+    await axios.get(CURRENT_WEATHER)
+    // await getWeatherApi(payload)
+      .then((response) => {
+        context.commit("UPDATE_WEATHER", response.data);
+        context.commit("UPDATE_TEMPERATURE", response.data);
+        context.commit("UPDATE_CITY_NAME", response.data.city_name);
       })
-      .catch(error => {
-        console.log("Error", error);
+      .catch((error) => {
+        console.log(error);
+        throw Error("API Error occurred.")
       });
-  }
+  },
 };
