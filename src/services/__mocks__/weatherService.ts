c7b24234bdc391437ff1ae7f415f51cd629030ad
Sophie van Wersch <sophievanwersch@hotmail.com>
2020-06-05 10:44:51 +0200

    Temporarily comment out weatherService api and mock

---
+++ src/services/__mocks__/weatherService.ts
@@ -0,0 +1,3 @@
+// export const mock = {
+//     get: jest.fn(() => new Promise(res => res({ data: [{city: "Amsterdam", country_code: "NL"}] })))
+// }
--- src/services/weatherService.ts
+++ src/services/weatherService.ts
@@ -1,15 +1,15 @@
-import axios from "axios";
-import { API_KEY } from "@/common/constant";
+// import axios from "axios";
+// // import { API_KEY } from "@/common/constant";
 
-export function getWeatherApi(payload: { country: string; city: string }) {
-  const { country, city } = payload;
-  const CURRENT_WEATHER = `https://api.weatherbit.io/v2.0/forecast/daily/current?city=${city},${country}&key=${API_KEY}&days=7`;
-  return axios
-    .get(CURRENT_WEATHER)
-    .then(response => {
-      return response.data;
-    })
-    .catch(error => {
-      console.log(error);
-    });
-}
+// export function getWeatherApi(payload: { countrySelected: string; city: string }) {
+//   const { countrySelected, city } = payload;
+//   const CURRENT_WEATHER = `https://api.weatherbit.io/v2.0/forecast/daily/current?city=${city},${countrySelected}&key=c9d0f8edb16446578c9047b2eda26642&days=7`;
+//   return axios
+//     .get(CURRENT_WEATHER)
+//     .then(response => {
+//       return response.data;
+//     })
+//     .catch(error => {
+//       console.log(error);
+//     });
+// }
